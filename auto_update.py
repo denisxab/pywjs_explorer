f694336fe2bc4e3d65e616af75a4318c0bb71a0e
Your Name <you@example.com>
2022-11-27 12:16:22 +0300

    "Вс 27 ноя 2022 12:16:22" -

--- auto_update.py
+++ auto_update.py
@@ -18,21 +18,21 @@ def syncGit():
     select_branch: str = subprocess.run(
         'git --no-pager branch --no-color --show-current', shell=True,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8'
-    ).stdout
+    ).stdout.replace('\n', '')
     if not select_branch:
         raise ValueError("Пустая ветка")
     # Проверить синхронизацию текущего локального проекта(даже если изменения небыли за комичены), с удаленной веткой.
     diff: str = subprocess.run(
         f'git --no-pager diff {origin}/{select_branch} --raw --name-status', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8'
-    ).stdout
+    ).stdout.replace('\n', '')
     if diff:
         #
         # Если есть различия в локальной и удаленной ветки
         #
         # Делаем коммит текущих локальных изменений.
-        subprocess.run('git add -A', check=True)
+        subprocess.run('git add -A', shell=True, check=True)
         subprocess.run(
-            f"git commit -m 'CommitByAutoUpdate:{datetime.now()}'", check=True)
+            f"git commit -m 'CommitByAutoUpdate:{datetime.now()}'", shell=True,check=True)
         # Получаем всю информацию об изменениях на удаленной ветки.
         subprocess.run('git fetch --all', shell=True, check=True)
         # Принудительно(во всех спорных случая берем данные из удаленной ветки) синхронизируем локальную ветку с удаленной.
